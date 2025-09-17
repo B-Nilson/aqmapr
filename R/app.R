d55af1688d1c0e58b63f379a6f3732add2c5481e
B-Nilson <nilson.brayden@gmail.com>
2025-09-17 09:37:22 -0700

    split data routing so that :type is specified last

--- R/app.R
+++ R/app.R
@@ -4,7 +4,11 @@ source("get_map.R")
 
 app <- ambiorix::Ambiorix$new(port = 8000L, host = "127.0.0.1")
 
-app$get("/data/:name/:type/:network/:site_id", get_data)
+# i.e. /data/recent/json
+app$get("/data/:name/:type", get_data)
+
+# i.e. /data/plotting/agency/10102/json
+app$get("/data/:name/:network/:site_id/:type", get_data)
 
 app$get("/", get_map)
 
