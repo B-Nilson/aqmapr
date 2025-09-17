5403bbd4400d91e3a17d6597c3f0541eefb0ccad
B-Nilson <nilson.brayden@gmail.com>
2025-09-16 21:58:52 -0700

    add current aqmap data to map

--- R/app.R
+++ R/app.R
@@ -2,7 +2,9 @@ source("functions.R")
 app <- ambiorix::Ambiorix$new(port = 8000L, host = "127.0.0.1")
 
 app$get("/", \(req, res) {
-  map <- make_leaflet_map()
+  recent_aqmap_data <- load_recent_aqmap_data() |> 
+    handyr::on_error(.return = NULL)
+  map <- make_leaflet_map(marker_data = recent_aqmap_data)
   res$htmlwidget(map)
 })
 
--- R/functions.R
+++ R/functions.R
@@ -1,4 +1,45 @@
-make_leaflet_map <- function() {
-  leaflet::leaflet() |> 
+make_leaflet_map <- function(marker_data = NULL) {
+  map <- leaflet::leaflet() |>
     leaflet::addProviderTiles(leaflet::providers$OpenStreetMap)
+
+  if (!is.null(marker_data)) {
+    map <- base_map |>
+      leaflet::addMarkers(
+        data = marker_data,
+        lng = ~lng,
+        lat = ~lat,
+        icon = ~leaflet::icons(icon_url, iconWidth = 32, iconHeight = 32)
+      )
+  }
+
+  return(map)
+}
+
+# Download and read in most recent AQmap obs datafile
+load_recent_aqmap_data <- function(data_dir = "../data") {
+  aqmap_url <- "https://aqmap.ca/aqmap"
+  file_name <- "aqmap_most_recent_obs.Rds"
+  local_path <- file.path(data_dir, file_name)
+  desired_cols <- c(
+    site_id = "sensor_index",
+    name = "monitor",
+    "network",
+    "lat",
+    "lng",
+    "prov_terr",
+    date_last_obs = "date",
+    "pm25_1hr",
+    icon_url = "icon_url_1hr"
+  )
+
+  # Download .rds file
+  aqmap_url |>
+    file.path("data", file_name) |>
+    download.file(local_path, mode = "wb")
+
+  # Load and cleanup
+  readRDS(local_path) |>
+    dplyr::select(dplyr::any_of(desired_cols)) |>
+    dplyr::filter(!is.na(pm25_1hr)) |>
+    dplyr::mutate(icon_url = file.path(aqmap_url, icon_url))
 }
