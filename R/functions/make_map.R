738d4e89c587894336a4f83a7ad51370284dbe5c
B-Nilson <nilson.brayden@gmail.com>
2025-09-17 12:12:18 -0700

    put missing data markers on seperate pane below other obs and make their icons smaller

--- R/functions/make_map.R
+++ R/functions/make_map.R
@@ -4,15 +4,26 @@ make_leaflet_map <- function(marker_data = NULL) {
     leaflet::addProviderTiles(leaflet::providers$OpenStreetMap)
 
   if (!is.null(marker_data)) {
+    icon_size <- list("obs" = 32, "missing_obs" = 16)
+    marker_data <- marker_data |>
+      dplyr::mutate(
+        pane = is.na(pm25_1hr) |>
+          ifelse(names(icon_size)[2], names(icon_size)[1]),
+        icon_width = unname(unlist(icon_size[pane])),
+        icon_height = icon_width
+      )
     # TODO: add default icon_url if value/column is missing
     map <- map |>
+      leaflet::addMapPane(names(icon_size)[2], zIndex = 650) |>
+      leaflet::addMapPane(names(icon_size)[1], zIndex = 660) |>
       leaflet::addMarkers(
         data = marker_data,
         group = ~as.character(network) |> 
           pretty_text(), 
+        options = ~ leaflet::pathOptions(pane = pane),
         lng = ~lng,
         lat = ~lat,
-        icon = ~ leaflet::icons(icon_url, iconWidth = 32, iconHeight = 32)
+        icon = ~leaflet::icons(icon_url, iconWidth = icon_width, iconHeight = icon_height)
       ) |> 
       leaflet::addLayersControl(
         overlayGroups = levels(marker_data$network) |> 
