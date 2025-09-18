918f10d87f048b13161e6f78c904cb0835c4be0a
B-Nilson <nilson.brayden@gmail.com>
2025-09-17 21:58:30 -0700

    move add_monitor_legend() to monitors.R

--- R/functions/monitors.R
+++ R/functions/monitors.R
@@ -69,3 +69,61 @@ add_obs_markers <- function(map, marker_data) {
       labelOptions = hover_options
     )
 }
+
+add_monitor_legend = function(
+  map,
+  networks,
+  legend_details = list(
+    hover = "Monitor Types",
+    title = "Monitor Types"
+  ),
+  position = "bottomright"
+) {
+  # Make icon paths
+  network_icons <- networks |>
+    make_marker_icon_path(
+      pm25_1hr = rep(-1, length(networks)),
+      local = FALSE
+    ) |>
+    setNames(networks)
+
+  # Make legend title
+  title <- legend_details$title |>
+    htmltools::tags$strong() |>
+    htmltools::tags$span(title = legend_details$hover)
+
+  # Make icon references
+  style <- "vertical-align: middle; max-width: %spx; max-height: %spx;" |>
+    sprintf(.marker_sizes$legend, .marker_sizes$legend)
+  icons <- network_icons |>
+    handyr::for_each(
+      \(pth) htmltools::tags$img(src = pth, style = style),
+      .name = TRUE,
+      .quiet = TRUE
+    )
+
+  # Make text for beside each icon
+  style <- "vertical-align: middle;"
+  texts <- networks |>
+    pretty_text() |>
+    handyr::for_each(htmltools::tags$span, style = style, .name = TRUE)
+
+  # Combine tags
+  legend <- title |>
+    htmltools::tags$div(
+      htmltools::tags$br(),
+      icons$agency,
+      texts$agency,
+      htmltools::tags$br(),
+      icons$lcm,
+      texts$lcm,
+    )
+    
+  # Add to map
+  map |>
+    leaflet::addControl(
+      html = legend,
+      layerId = "monitor_legend",
+      position = position
+    )
+}
