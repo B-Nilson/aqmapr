0c33fca13731badf096ffe2e6505d3bdac2a2f92
B-Nilson <nilson.brayden@gmail.com>
2025-10-14 21:56:09 -0700

    expand tests

--- tests/testthat/_snaps/make_leaflet_map.md
+++ tests/testthat/_snaps/make_leaflet_map.md
@@ -3,9 +3,18 @@
     Code
       expect_no_warning(expect_no_error(leaflet::addLegend(make_leaflet_map(
         point_data = list(Communities = canada_communities), point_options = list(
-          radius = 3, weight = 1, color = "black", fillColor = ~ leaflet::colorFactor(
-            "viridis", domain = name)(name), fillOpacity = 0.8, opacity = 1, label = ~
-          lapply(paste("Name: ", name, "<br/>", "Type: ", type), htmltools::HTML))),
-      pal = colour_pal, values = factor(levels(canada_communities$type), levels = levels(
-        canada_communities$type)))))
+          radius = 3, weight = 1, color = "black", fillColor = ~ colour_pal(type),
+          fillOpacity = 0.8, opacity = 1, label = ~ lapply(paste("Name: ", name,
+            "<br/>", "Type: ", type), htmltools::HTML))), pal = colour_pal, values = sort(
+        unique(canada_communities$type)))))
+
+# advanced case with points data works
+
+    Code
+      expect_no_warning(expect_no_error(leaflet::addLegend(make_leaflet_map(
+        point_data = split(canada_communities, canada_communities$type),
+        point_options = list(radius = 3, weight = 1, color = "black", fillColor = ~
+          colour_pal(type), fillOpacity = 0.8, opacity = 1, label = ~ lapply(paste(
+            "Name: ", name, "<br/>", "Type: ", type), htmltools::HTML))), pal = colour_pal,
+      values = sort(unique(canada_communities$type)))))
 
--- tests/testthat/test-make_leaflet_map.R
+++ tests/testthat/test-make_leaflet_map.R
@@ -1,12 +1,12 @@
 test_that("basic case with points data works", {
-  colour_pal <- leaflet::colorFactor("viridis", domain = canada_communities$type, ordered = TRUE)
+  colour_pal <- leaflet::colorFactor("viridis", domain = levels(canada_communities$type), ordered = TRUE, reverse = TRUE)
   make_leaflet_map(
     point_data = list("Communities" = canada_communities),
     point_options = list(
       radius = 3,
       weight = 1,
       color = "black",
-      fillColor = ~ leaflet::colorFactor("viridis", domain = name)(name),
+      fillColor = ~colour_pal(type),
       fillOpacity = 0.8,
       opacity = 1,
       label = ~ paste("Name: ", name, "<br/>", "Type: ", type) |>
@@ -15,8 +15,31 @@ test_that("basic case with points data works", {
   ) |>
     leaflet::addLegend(
       pal = colour_pal,
-      values = levels(canada_communities$type) |>
-        factor(levels = levels(canada_communities$type))
+      values = unique(canada_communities$type) |> sort()
+    ) |> 
+      expect_no_error() |> 
+      expect_no_warning() |> 
+      expect_snapshot()
+})
+
+test_that("advanced case with points data works", {
+  colour_pal <- leaflet::colorFactor("viridis", domain = levels(canada_communities$type), ordered = TRUE, reverse = TRUE)
+  make_leaflet_map(
+    point_data = canada_communities |> split(canada_communities$type),
+    point_options = list(
+      radius = 3,
+      weight = 1,
+      color = "black",
+      fillColor = ~colour_pal(type),
+      fillOpacity = 0.8,
+      opacity = 1,
+      label = ~ paste("Name: ", name, "<br/>", "Type: ", type) |>
+        lapply(htmltools::HTML)
+    )
+  ) |>
+    leaflet::addLegend(
+      pal = colour_pal,
+      values = unique(canada_communities$type) |> sort()
     ) |> 
       expect_no_error() |> 
       expect_no_warning() |> 
