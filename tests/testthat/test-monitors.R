3f08b15f75f0c0fd8c225f6c6d854a863738fe15
B-Nilson <nilson.brayden@gmail.com>
2025-10-14 21:26:24 -0700

    stop -> skip

--- tests/testthat/test-monitors.R
+++ tests/testthat/test-monitors.R
@@ -1,5 +1,5 @@
 test_that("add_obs_markers() works", {
-  stop("Requires server to be running")
+  skip("Requires server to be running")
   map <- leaflet::leaflet()
   networks <- c("agency", "lcm", "purpleair", "aqegg")
   map |>
