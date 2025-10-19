4cca58e9b14aef371878de924f0ec43f1ba80d55
B-Nilson <nilson.brayden@gmail.com>
2025-10-18 21:27:12 -0700

    create, document, export, and test get_eccc_eer_smoke_forecasts

---
+++ R/get_eccc_eer_smoke_forecasts.R
@@ -0,0 +1,180 @@
+#' Get ECCC EER smoke forecasts
+#'
+#' The Environmental Emergency Response group at Environment and Climate Change Canada (ECCC) provides smoke forecasts for Canada.
+#' They run dispersion models using fire information and ECCC weather models.
+#' See \href{https://eer.cmc.ec.gc.ca/mandats/AutoSim/Fire/}{here} for more information.
+#'
+#' @param select_times POSIXct time(s) to download data for
+#' @param region String specifying the region to download data for. Currently only "Canada" is supported.
+#' @param data_dir Directory to store downloaded data in. Defaults to tempdir().
+#' @param quiet Logical. Should output from `download.file` be suppressed?
+#' @export
+#'
+#' @examples
+#' eer <- get_eccc_eer_smoke_forecasts()
+#' make_leaflet_map(
+#'   polygon_data = list("EER Smoke" = eer),
+#'   polygon_options = list(
+#'     fillColor = ~ leaflet::colorFactor(palette = "viridis", unique(min_pm25))(
+#'       min_pm25
+#'     ),
+#'     weight = 2,
+#'     color = "black",
+#'     fillOpacity = 0.8,
+#'     opacity = 1
+#'   )
+#' )
+get_eccc_eer_smoke_forecasts <- function(
+  select_times = Sys.time(),
+  region = "Canada",
+  data_dir = tempdir(),
+  quiet = FALSE
+) {
+  # cat(paste0('c("', paste(get_eer_regions(Sys.time()), collapse = '", "'), '")'))
+  stopifnot(lubridate::is.POSIXct(select_times), length(select_times) > 0)
+  stopifnot(
+    region %in%
+      c(
+        "Canada",
+        "AB_SK_North",
+        "AB_SK_South",
+        "Atlantic_East",
+        "Atlantic_Labrador",
+        "Atlantic_West",
+        "BC_AB_North",
+        "BC_North",
+        "BC_South",
+        "BC_South_Cranbrook",
+        "BC_South_Lytton",
+        "BC_South_Williams_Lake",
+        "Great_Lakes",
+        "MB_ON_South",
+        "NB_CFB_Gagetown",
+        "NS_South",
+        "NT",
+        "NU",
+        "ON_North",
+        "QC_Central",
+        "QC_North",
+        "QC_South",
+        "SK_MB_North",
+        "SK_MB_South",
+        "YT",
+        "YT_North"
+      )
+  )
+  stopifnot(is.character(data_dir), length(data_dir) == 1)
+
+  desired_cols <- c(
+    "region",
+    "model_time",
+    "forecast_time",
+    min_pm25 = "Interval",
+    altitude = "Height"
+  )
+
+  # Floor to nearest UTC hour
+  select_times <- select_times |>
+    lubridate::with_tz("UTC") |>
+    lubridate::floor_date("hours") |>
+    unique()
+
+  # Download and unzip new runs as needed
+  select_times |> get_eer_zip(data_dir = data_dir, quiet = quiet)
+
+  # Build shp file name path
+  shape_names <- "shp_%s_%s" |>
+    sprintf(region, format(select_times, "%Y%m%d-%H%M"))
+  shape_paths <- data_dir |>
+    file.path(shape_names, paste0(shape_names, ".shp"))
+
+  # Load shapefile, convert to POLYGON and cleanup
+  eer_smoke <- shape_paths |>
+    handyr::for_each(
+      \(path, i) {
+        path |>
+          sf::read_sf() |>
+          # Add useful info
+          dplyr::mutate(
+            Height = Height |> units::set_units("m"),
+            model_time = select_times[i] |>
+              lubridate::floor_date("6 hours"),
+            forecast_time = select_times[i],
+            region = region
+          )
+      },
+      .bind = TRUE,
+      .show_progress = FALSE,
+      .enumerate = TRUE
+    ) |>
+    dplyr::filter(!sf::st_is_empty(geometry)) |>
+    # LINESTRING -> POLYGON
+    sf::st_cast("POLYGON") |>
+    sf::st_make_valid() |>
+    # Select/rename columns
+    dplyr::select(dplyr::all_of(desired_cols))
+
+  # Remove overlap of polygons so opacity works properly
+  eer_smoke |>
+    split(~forecast_time) |>
+    handyr::for_each(
+      \(fcst_data) {
+        fcst_data |>
+          dplyr::arrange(dplyr::desc(min_pm25)) |>
+          sf::st_transform(3857) |>
+          sf::st_difference() |>
+          sf::st_transform("WGS84") |>
+          dplyr::arrange(min_pm25)
+      },
+      .bind = TRUE,
+      .show_progress = FALSE
+    )
+}
+
+get_eer_zip <- function(select_times, data_dir = tempdir(), unzip = TRUE, quiet = FALSE) {
+  # Model runs are updated every 6 hours
+  model_runs <- select_times |>
+    lubridate::floor_date("6 hours") |>
+    unique()
+  is_todays <- model_runs >= lubridate::with_tz(Sys.Date(), "UTC")
+
+  # Build url to this runs zip file
+  zip_urls <- model_runs |>
+    make_eer_zip_dir() |>
+    paste0("shp_Canada.zip")
+
+  # Download and unzip new runs as needed
+  run_zips <- data_dir |>
+    file.path(
+      paste0("eer_", format(model_runs, "%Y%m%d_%H00"), "_shp_Canada.zip")
+    )
+  zip_urls |>
+    handyr::for_each(.enumerate = TRUE, \(zip_url, i) {
+      if (is_todays[i] || !file.exists(run_zips[i])) {
+        zip_url |> download.file(destfile = run_zips[i], mode = "wb", quiet = quiet)
+        if (unzip) unzip(run_zips[i], exdir = data_dir)
+      }
+    })
+}
+
+make_eer_zip_dir <- function(model_runs) {
+  is_todays <- model_runs >= lubridate::with_tz(Sys.Date(), "UTC")
+  source_template <- "https://eer.cmc.ec.gc.ca/mandats/AutoSim/Fire/%s/Canada/%s/shp/"
+  timestamps <- is_todays |>
+    ifelse("latest", format(model_runs, "%Y%m%d.%H00"))
+  source_template |>
+    sprintf(format(model_runs, "%HUTC"), timestamps) |>
+    unique()
+}
+
+get_eer_regions <- function(select_time) {
+  zip_dir <- select_time |>
+    lubridate::floor_date("6 hours") |>
+    make_eer_zip_dir()
+  zip_dir |>
+    url() |>
+    readLines() |>
+    stringr::str_subset("href=\".*zip\"") |>
+    stringr::str_extract("href=\"shp_(.*?).zip\"", group = 1) |>
+    unique()
+}
---
+++ tests/testthat/test-get_eccc_eer_smoke_forecasts.R
@@ -0,0 +1,10 @@
+test_that("basic case works", {
+  eer <- get_eccc_eer_smoke_forecasts() |> 
+    expect_no_error() |> 
+    expect_no_warning()
+
+  expect_equal(
+    names(eer),
+    c("region", "model_time", "forecast_time", "min_pm25", "altitude", "geometry")
+  )
+})
