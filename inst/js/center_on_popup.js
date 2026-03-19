5f11dd7fe5a70f05fa9b2a37c5c37d520ebf76c8
Brayden Nilson <nilson.brayden@gmail.com>
2026-03-19 14:35:04 -0700

    get openpopup from map instead of as arg

--- inst/js/center_on_popup.js
+++ inst/js/center_on_popup.js
@@ -1,6 +1,6 @@
-center_on_popup = function (map, open_popup, zoom) {
+center_on_popup = function (map, zoom) {
     if (zoom) map.setZoom(zoom);
-    var px = map.project(open_popup._latlng);
-    px.y -= open_popup._container.clientHeight / 2;
+    var px = map.project(map._popup._latlng);
+    px.y -= map._popup._container.clientHeight / 2;
     map.panTo(map.unproject(px))
 }
